f341700ce19b88e5a6113da88822cba5c6023aae
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-22 14:28:56 -0500

    Cards and Card re-factored using Styled Components (STRETCH COMPLETED)

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,19 +1,63 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import styled from 'styled-components'
 
 const Card = props => {
   return (
-    <div className="card">
-      <div className="headline">{props.card.headline}</div>
-      <div className="author">
-        <div className="img-container">
-          <img src={props.card.img} alt='doggo'/>
-        </div>
-        <span>By {props.card.author}</span>
-      </div>
-    </div>
+    <CardContainer>
+      <Headline>{props.card.headline}</Headline>
+      <AuthorContainer>
+        <ImgContainer>
+          <AuthorImg src={props.card.img} alt='doggo'/>
+        </ImgContainer>
+        <Author>By {props.card.author}</Author>
+      </AuthorContainer>
+    </CardContainer>
   );
 };
+
+const CardContainer = styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: space-between;
+  align-items: none;
+  flex-direction: column;
+  background-color: #fff;
+  width: 380px;
+  margin-bottom: 16px;
+  padding: 24px;
+`;
+
+const Headline = styled.div`
+  font-size: 25px;
+  font-family: Didot, serif;
+`;
+
+const AuthorContainer = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  margin-top: 15px;
+`;
+
+const ImgContainer = styled.div`
+  padding-right: 10px;
+  border-right: 1px solid lightgrey;
+  height: 40px;
+`;
+
+const AuthorImg = styled.img`
+  width: 40px;
+`;
+
+const Author= styled.span`
+  padding-left: 10px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+`;
+
 
 Card.propTypes = {
   card: PropTypes.shape({
