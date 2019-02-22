7093f787c6371a6c254b302af2c7ea76386aff0d
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-22 13:09:59 -0500

    Re-factoring Header component using styled-components

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components'
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
@@ -12,5 +13,47 @@ const Header = () => {
     </div>
   )
 }
+
+const HeaderContainer = styled.div`
+  display: flex,
+  justify-content: space-between,
+  align-items: center,
+  flex-direction: row,
+  background-color: #fff,
+  border-bottom: 1px solid lightgrey,
+  width: 100%,
+  height: 120px,
+  margin-top: 44px,
+  padding-bottom: 15px,
+
+  @media (min-width: 1280px) {
+      width: 1280px,
+  }
+`;
+
+const HeaderTitle = styled.h1`
+    font-size: 60px,
+    font-family: Didot, serif,
+    font-weight: bold,
+    flex: 8,
+    text-align: center,
+    color: #000,
+`;
+
+.header span `
+  align-self: flex-end,
+  font-size: 11px,
+  font-weight: bold,
+  letter-spacing: 1px,
+`;
+.header .date `
+  margin-left: 25px,
+  flex: 1,
+`;
+.header .temp `
+  text-align: right,
+  margin-right: 25px,
+  flex: 1,
+`;
 
 export default Header
