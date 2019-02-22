876fe6c0ae44a15e8c4bb4afa82835fda2820d9f
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-22 13:01:47 -0500

    Added type checking to Tab component

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
@@ -27,6 +28,9 @@ const Tab = props => {
   }
 };
 
-// Make sure you include PropTypes on your props.
-
+Tab.propTypes = {
+  tab: PropTypes.string,
+  selectTabHandler: PropTypes.func,
+  selectedTab: PropTypes.func
+}
 export default Tab;
