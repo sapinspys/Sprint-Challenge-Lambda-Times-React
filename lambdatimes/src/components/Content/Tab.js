630b349bafddb8fb2a3c8fa5f369e671c67a3f92
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-22 14:20:41 -0500

    Tabs and tab re-factored with Styled Components

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import styled from 'styled-components'
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
@@ -7,26 +8,64 @@ const Tab = props => {
       if it is not it should just be 'tab'*/
   if (props.tab !== props.selectedTab) {
     return (
-      <div
-        className={'tab'}
+      <RegularTab
         onClick={() => props.selectTabHandler(props.tab)}
           /* Replace this dummy click handler function with your selectTabHandler function from props 
            you'll need to pass the `tab` in as an argument to this handler. */ >
         {props.tab.toUpperCase()}
-      </div>
+      </RegularTab>
     );
   } else {
     return (
-      <div
-        className={`tab active-tab`}
+      <ActiveTab
         onClick={() => props.selectTabHandler(props.tab)}
           /* Replace this dummy click handler function with your selectTabHandler function from props 
            you'll need to pass the `tab` in as an argument to this handler. */ >
         {props.tab.toUpperCase()}
-      </div>
+      </ActiveTab>
     );
   }
 };
+
+const RegularTab = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  margin: 0 5px;
+  padding: 2px 10px;
+  font-size: 12px;
+  letter-spacing: 2px;
+  cursor: pointer;
+  font-weight: bold;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const ActiveTab = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  color: #333;
+  background-color: #fff;
+  margin: 0 5px;
+  padding: 2px 10px;
+  font-size: 12px;
+  letter-spacing: 2px;
+  cursor: pointer;
+  font-weight: bold;
+  border: 2px solid #333;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
 
 Tab.propTypes = {
   tab: PropTypes.string,
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Tab from './Tab';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import styled from 'styled-components'
 
 const Tabs = props => {
   return (
-    <div className="tabs">
-      <div className="topics">
-        <span className="title">TRENDING TOPICS:</span>
+    <TabsContainer>
+      <Topics>
+        <TopicTitle>TRENDING TOPICS:</TopicTitle>
           {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
           {props.tabs.map((tab, index) => (
@@ -15,10 +16,38 @@ const Tabs = props => {
               selectTabHandler={props.selectTabHandler}
               selectedTab={props.selectedTab} />
           ))}
-      </div>
-    </div>
+      </Topics>
+    </TabsContainer>
   );
 };
+
+const TabsContainer = styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  width: 100%;
+  height: 47px;
+  background-color: #fff;
+
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
+
+const Topics = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+`;
+
+const TopicTitle = styled.span`
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+`;
 
 Tabs.propTypes = {
   tabs: PropTypes.arrayOf(PropTypes.string)
